0284c1d6bca5f6577cf3002c8b32de62b32f4b50
Philip Ziolkowski <philipz4848@gmail.com>
2023-04-30 13:15:42 -0700

    Adding activity tests

--- src/__tests__/integration/Itinerary.test.tsx
+++ src/__tests__/integration/Itinerary.test.tsx
@@ -3,12 +3,12 @@ import userEvent from '@testing-library/user-event'
 import TripDay from '../../components/TripDay'
 
 describe('Activity operations', () => {
-    test('Able to create a new activity', async () => {
+    test('Able to create an activity', async () => {
         render(<TripDay date={new Date()} activities={[]} tripDayId={10}/>)
 
         // Get input field and enter text
         const activityNameInput = screen.getByLabelText('activity-name-input')
-        await userEvent.type(activityNameInput, 'Eiffel')
+        await userEvent.type(activityNameInput, 'River')
 
         // Get add acitivty button and click it
         const addActivityButton = screen.getByRole('button', {name: 'add-activity-button'})
@@ -16,7 +16,42 @@ describe('Activity operations', () => {
 
         // Expect new activity with text as title to exist
         setTimeout(() => {
-            expect(screen.getByText('Eiffel')).toBeDefined()
-        }, 1000)
+            expect(screen.getByText('River')).toBeDefined()
+        }, 1500)
+    })
+
+    test('Activity cannot be created without a name', async () => {
+        render(<TripDay date={new Date()} activities={[]} tripDayId={10}/>)
+
+        // Get add activity button and click it
+        const addActivityButton = screen.getByRole('button', {name: 'add-activity-button'})
+        await userEvent.click(addActivityButton)
+
+        setTimeout(() => {
+            expect(screen.getByText('Add time')).toBeUndefined()
+        }, 1500)
+    })
+
+    test('Able to delete an activity', async () => {
+        // Create an itinerary
+        render(<TripDay date={new Date()} activities={[]} tripDayId={10}/>)
+
+        // Get input field and enter text
+        const activityNameInput = screen.getByLabelText('activity-name-input')
+        await userEvent.type(activityNameInput, 'River')
+
+        // Get add acitivty button and click it
+        const addActivityButton = screen.getByRole('button', {name: 'add-activity-button'})
+        await userEvent.click(addActivityButton)
+
+        // Get delete icon and click it
+        setTimeout(async () => {
+            await userEvent.click(screen.getByRole('button', {name: 'delete-button'}))
+        }, 1500)
+        
+        // Expect new activity to be deleted
+        setTimeout(() => {
+            expect(screen.getByText('Add time')).toBeUndefined()
+        }, 1500)
     })
 })
