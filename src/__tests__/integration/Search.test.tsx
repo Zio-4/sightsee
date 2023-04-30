098dae8c78b52e0faf05d3e765cebab8baf80fb3
Philip Ziolkowski <philipz4848@gmail.com>
2023-04-30 15:20:17 -0700

    search test

---
+++ src/__tests__/integration/Search.test.tsx
@@ -0,0 +1,10 @@
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+describe('Search tests', () => {
+    test('Can search', () => {
+        // render(<Discover/>)
+    })
+
+    test('Search works if nothing is found')
+})
